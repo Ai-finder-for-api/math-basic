0684629d78b7621aa882761cb3b619f6ca76d9e7
Yash <yashjoshidpsbangalore@gmail.com>
2026-08-04 07:00:13 +0530

    v0.4.3: Restore batch_process_vectors function

--- src/math_basic/bindings/PyVec.cpp
+++ src/math_basic/bindings/PyVec.cpp
@@ -3,6 +3,17 @@
 
 namespace py = pybind11;
 
+// The missing batch function!
+py::list batch_process_vectors(py::list vectors) {
+    py::list results;
+    for (auto item : vectors) {
+        Vec4& v1 = item.cast<Vec4&>();
+        Vec4 scaled = v1 * 2.0f;
+        results.append(scaled.dot(scaled));
+    }
+    return results;
+}
+
 void init_vec(py::module_ &m) {
     py::class_<Vec4>(m, "Vec4", py::buffer_protocol())
         .def(py::init<>())
@@ -13,4 +24,7 @@ void init_vec(py::module_ &m) {
         .def("__mul__", &Vec4::operator*, py::is_operator())
         .def("dot", &Vec4::dot)
         .def("length_sq", &Vec4::length_sq);
+
+    // Bind it to the module
+    m.def("batch_process_vectors", &batch_process_vectors, "Processes a list of vectors in C++");
 }
