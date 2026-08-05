690a7cd42036ece9df17d960239da8511d5add51
Yash <yashjoshidpsbangalore@gmail.com>
2026-08-05 18:16:05 +0530

    fix: resolve MSVC flags, remove duplicate Vec4, add missing pybind exports

--- src/math_basic/bindings/PyQuantum.cpp
+++ src/math_basic/bindings/PyQuantum.cpp
@@ -1,23 +1,39 @@
 #include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
 #include "../math/quantum/Gate.hpp"
 #include "../math/quantum/Pauli.hpp"
 #include "../math/quantum/Hadamard.hpp"
 #include "../math/quantum/CNOT.hpp"
 #include "../math/quantum/Tensor.hpp"
+#include "core/ComplexMatrix.hpp"
+#include "core/QuantumState.hpp"
 
 namespace py = pybind11;
 using namespace qmath;
 
-// Helper to bind generic QState and QGate
 void init_quantum(py::module_ &m) {
-    // Complex number binding
     py::class_<Complex<float>>(m, "Complex")
         .def(py::init<float, float>())
         .def_readwrite("real", &Complex<float>::real)
         .def_readwrite("imag", &Complex<float>::imag)
         .def("__mul__", &Complex<float>::operator*, py::is_operator());
 
-    // 2x2 Gate and State
+    py::class_<ComplexMatrix>(m, "ComplexMatrix", py::buffer_protocol())
+        .def(py::init<int, int>())
+        .def("set", &ComplexMatrix::set)
+        .def("get", &ComplexMatrix::get)
+        .def_property_readonly("rows", &ComplexMatrix::get_rows)
+        .def_property_readonly("cols", &ComplexMatrix::get_cols)
+        .def("multiply", &ComplexMatrix::multiply)
+        .def_buffer([](ComplexMatrix &mat) -> py::buffer_info {
+            return mat.get_buffer_info();
+        });
+
+    py::class_<QuantumState>(m, "QuantumState")
+        .def(py::init<int>())
+        .def("apply_gate", &QuantumState::apply_gate)
+        .def("get_state", &QuantumState::get_state, py::return_value_policy::reference_internal);
+
     py::class_<QGate<2, float>>(m, "QGate2")
         .def(py::init<>())
         .def("get", &QGate<2, float>::get)
@@ -28,7 +44,6 @@ void init_quantum(py::module_ &m) {
         .def("apply", &QState<2, float>::apply)
         .def("get", &QState<2, float>::get);
 
-    // 4x4 Gate and State (2 qubits)
     py::class_<QGate<4, float>>(m, "QGate4")
         .def(py::init<>())
         .def("get", &QGate<4, float>::get)
@@ -40,13 +55,10 @@ void init_quantum(py::module_ &m) {
         .def("apply", &QState<4, float>::apply)
         .def("get", &QState<4, float>::get);
 
-    // Quantum Gate Factories
     m.def("pauli_x", &make_pauli_x<float>);
     m.def("pauli_y", &make_pauli_y<float>);
     m.def("pauli_z", &make_pauli_z<float>);
     m.def("hadamard", &make_hadamard<float>);
     m.def("cnot", &make_cnot<float>);
-    
-    // Tensor Product
     m.def("tensor_product", &tensor_product<2, 2, float>);
 }
